ca7be46994f9c791923b79b86742340411ebe31e
Ajay23 <ajaysarode2121@gmail.com>
2022-11-20 08:46:40 +0530

    Introduction to Print Statement in Java.

---
+++ Intro.java
@@ -0,0 +1,17 @@
+public class Intro {
+    public static void main(String args[])
+    {
+        System.out.println("Hello, Ajay");
+
+        int MyFirstNumber = (10 + 5) + (2 * 10);
+        int MySecondNumber = 10;
+        int MyThirdNumber = 5;
+
+        int Total = MyFirstNumber + MySecondNumber + MyThirdNumber;  //calculating 3 variable and storing them in one variable that is Total
+        System.out.println(Total);  //printing Total
+
+        int MyLastOne = 1000 - Total;
+        System.out.println(MyLastOne);
+    }
+
+}
