2d1d706766609b1e8387b695b16ac320bf24636e
Ajay23 <ajaysarode2121@gmail.com>
2022-11-22 19:43:32 +0530

    Ruff Work

---
+++ Ruff.java
@@ -0,0 +1,11 @@
+public class Ruff {
+
+    public static void main(String[] args){
+
+        String FirstString = "My name is ajay";
+        System.out.println("Before Changing The Value = " + FirstString);
+
+        FirstString = "hello";
+        System.out.println("After Changing The Value = " + FirstString);
+    }
+}
