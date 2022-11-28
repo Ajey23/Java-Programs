7311c173dd74de3fc442ad1ea5462b873792994a
Ajay23 <ajaysarode2121@gmail.com>
2022-11-28 19:49:07 +0530

    Printing Prime Number.

---
+++ PrimeNumber.java
@@ -0,0 +1,17 @@
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+
+        System.out.println("0 is " + (isPrime(0) ? "" : "Not ") + "a prime number" );
+        System.out.println("1 is " + (isPrime(1) ? "" : "Not ") + "a prime number" );
+        System.out.println("2 is " + (isPrime(2) ? "" : "Not ") + "a prime number" );
+        System.out.println("3 is " + (isPrime(3) ? "" : "Not ") + "a prime number" );
+    }
+    public static boolean isPrime(int WholeNumber) {
+
+        if (WholeNumber <= 2) {
+            return (WholeNumber == 2);
+        }
+        return true;
+    }
+}
